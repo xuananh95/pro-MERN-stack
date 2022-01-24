1173b1edbb2731ecf55d9a307898f76a737fbfd3
xuananh95 <xanh.trym95@gmail.com>
2022-01-24 17:04:51 +0700

    01-hello_world

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+var contentNode = document.getElementById('contents');
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const message = continents.map(c => `Hello ${c}! <br />`).join(' ');
+const component = <p>{message}</p>;
+ReactDOM.render(component, contentNode);
