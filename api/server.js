e83b2f328f32ddf2e07cab17cf28d28eb295ca11
xuananh95 <xanh.trym95@gmail.com>
2022-02-14 09:20:12 +0700

    07.01:UI Server

--- api/server.js
+++ api/server.js
@@ -91,7 +91,7 @@ async function issueAdd(_, { issue }) {
 }
 
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+  typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
   resolvers,
   formatError: error => {
     console.log(error);
@@ -101,7 +101,6 @@ const server = new ApolloServer({
 
 const app = express();
 
-app.use(express.static('public'));
 
 server.applyMiddleware({ app, path: '/graphql' });
 
@@ -109,7 +108,7 @@ server.applyMiddleware({ app, path: '/graphql' });
   try {
     await connectToDb();
     app.listen(3000, function () {
-      console.log('App started on port 3000');
+      console.log('API server started on port 3000');
     });
   } catch (err) {
     console.log('ERROR:', err);
